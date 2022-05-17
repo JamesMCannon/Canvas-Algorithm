5eac33011884a7e8f6ec051207c842f7ad931d3d
JamesMCannon <james.cannon-1@colorado.edu>
2022-05-17 16:41:21 -0600

    added new test frequencies; low, mid, and high amplitudes

--- fpgamodel.py
+++ fpgamodel.py
@@ -22,58 +22,60 @@ for f in files:
 
 # some set up parameters
 fs = 131072.                # sampling freq. in Hz
-signal_freq0 = 14000         # signal freq. 1 in Hz
+signal_freq0 = 24000        # signal freq. 1 in Hz
 signal_freq1 = 30e3         # signal freq. 2 in Hz
-amp0 = 2**15                # amplitudes (in ADC units)
+#amp0 = 2**11               # amplitudes (in ADC units)
+amp0 = 27345                # max amp of VHDL simulation signals
 amp1 = 2**15                # amplitudes (in ADC units)
 shift0 = 0                  # phase shift in radians
 shift1 = 0                  # phase shift in radians
-sample_len = 0.5              # seconds
+sample_len = 0.5            # seconds
 nFFT = 1024                 # length of FFT
 n_acc = 8                   # number of FFTs to accummulate
 
 # STEP 1 -------------------- GENERATE INPUT ----------------------------- 
 # get one or two test singals
-#channels0_td = test_signal(fs, sample_len, signal_freq0, amp0, shift=shift0, channel_num=0, show_plots=True, save_output='both')
-inputs = 'Inputs/'
-freq = '60kHz'
-file = inputs+'hi_amp_'+freq+'.txt'  
-channels0_td = read_FPGA_input(file,signed=True,show_plots=True)
+channels0_td = test_signal(fs, sample_len, signal_freq0, amp0, shift=shift0, channel_num=0, show_plots=False, save_output='both')
+#inputs = 'Inputs/'
+#freq = '60kHz'
+#file = inputs+'hi_amp_'+freq+'.txt'  
+#channels0_td = read_FPGA_input(file,signed=True,show_plots=True)
 #channels1_td = test_signal(fs, sample_len, signal_freq1, amp1, shift=shift1, channel_num=1, show_plots=False, save_output='both')
 
+#print(max(channels0_td))
 # STEP 2 ----------------- GET HANNING WINDOW ----------------------------
 # get a window based on IDL code
-win = get_win(nFFT, show_plots=True, save_output=None)
+win = get_win(nFFT, show_plots=False, save_output=None)
 
 # STEP 3 ----------------------- TAKE FFT --------------------------------
 # take fft on each channel
 
-channel0_fd_real, channel0_fd_imag = canvas_fft(nFFT, fs, win, channels0_td, overlap=True,  channel_num=0, show_plots=True, save_output='both')
+channel0_fd_real, channel0_fd_imag = canvas_fft(nFFT, fs, win, channels0_td, overlap=True,  channel_num=0, show_plots=False, save_output='both')
 #channel1_fd_real, channel1_fd_imag = canvas_fft(nFFT, fs, win, channels1_td, overlap=True,  channel_num=1, show_plots=False, save_output='both')
 
 # STEP 4 ----------------------- CALC PWR --------------------------------
 # calculate power, diff for spectra and x-spec
-fpga_rev = "Rev14p1"
-fpga_file_path = "Data_compare/FPGA_Results/"+fpga_rev+'/FPGA-'+fpga_rev+'_'+freq+'_FFT_hex.txt'
+#fpga_rev = "Rev14p1"
+#fpga_file_path = "Data_compare/FPGA_Results/"+fpga_rev+'/FPGA-'+fpga_rev+'_'+freq+'_FFT_hex.txt'
 
-f_ar, f_ai = read_FPGA_input_lines(fpga_file_path, 32, line_n=3, x=1, y=2)
+#f_ar, f_ai = read_FPGA_input_lines(fpga_file_path, 32, line_n=3, x=1, y=2)
 
-#spec_pwr0 = fft_spec_power(channel0_fd_real, channel0_fd_imag, channel_num=0, show_plots=True, save_output='both')
+spec_pwr0 = fft_spec_power(channel0_fd_real, channel0_fd_imag, channel_num=0, show_plots=False, save_output='both')
 #spec_pwr1 = fft_spec_power(channel1_fd_real, channel1_fd_imag, channel_num=1, show_plots=False, save_output='both')
 #xspec_pwr_r, xspec_pwr_i = fft_xspec_power(channel0_fd_real, channel0_fd_imag, channel1_fd_real, channel1_fd_imag, channel_nums=[0,1], show_plots=True, save_output='both')
 
-spec_pwr0 = fft_spec_power(f_ar, f_ai, channel_num=0, show_plots=True, save_output='both')
+#spec_pwr0 = fft_spec_power(f_ar, f_ai, channel_num=0, show_plots=True, save_output='both')
 #spec_pwr1 = fft_spec_power(f_ar, f_ai, channel_num=1, show_plots=True, save_output='both')
 #xspec_pwr_r, xspec_pwr_i = fft_spec_power([channel0_fd_real], [channel0_fd_imag], [channel1_fd_real], [channel1_fd_imag], channel_nums=[0,1], show_plots=False, save_output='both')
 # STEP 5 -------------------- rebin and acc -------------------------------
 # functions written to rebin (avg in freq) and acc (avg in time)
-rebin_pwr0= rebin_likefpga(spec_pwr0, channel_num=0, show_plots=True, save_output='both')
+rebin_pwr0= rebin_likefpga(spec_pwr0, channel_num=0, show_plots=False, save_output='both')
 #rebin_pwr1= rebin_likefpga(spec_pwr1, channel_num=1, show_plots=False, save_output=None)
 
 #rebin_pwr_01_r= rebin_likefpga(xspec_pwr_r, channel_num=0, show_plots=False, save_output='both')
 #rebin_pwr_01_i= rebin_likefpga(xspec_pwr_i, channel_num=0, show_plots=False, save_output='both')
 
-acc_pwr0 = acc_likefpga(rebin_pwr0, n_acc, channel_num=0, show_plots=True, save_output='both')
+acc_pwr0 = acc_likefpga(rebin_pwr0, n_acc, channel_num=0, show_plots=False, save_output='both')
 #acc_pwr1 = acc_likefpga(rebin_pwr1, n_acc, channel_num=1, show_plots=False, save_output='both')
 
 #acc_pwr01_r = acc_likefpga(rebin_pwr_01_r, n_acc, channel_num=0, show_plots=False, save_output='both')
@@ -87,7 +89,7 @@ fbins_dbl = [(float(f[0].replace(',','')),float(f[1].replace(',',''))) for f in 
 c_fbins = [item for sublist in fbins_dbl for item in sublist]
 center_freqs = [fs/nFFT * ff for ff in np.arange(0, 512)]
 
-avg_pwr0 = rebin_canvas(acc_pwr0, n_acc, c_fbins, center_freqs, tx_bins=True, channel_num=0, show_plots=True, save_output='both')
+avg_pwr0 = rebin_canvas(acc_pwr0, n_acc, c_fbins, center_freqs, tx_bins=True, channel_num=0, show_plots=False, save_output='both')
 #avg_pwr1 = rebin_canvas(acc_pwr1, n_acc, c_fbins, center_freqs, tx_bins=True, channel_num=1, show_plots=False, save_output='both')
 
 #avg_pwr01_r = rebin_canvas(acc_pwr01_r, n_acc, c_fbins, center_freqs, tx_bins=True, channel_num=0, show_plots=False, save_output='both')
@@ -95,7 +97,7 @@ avg_pwr0 = rebin_canvas(acc_pwr0, n_acc, c_fbins, center_freqs, tx_bins=True, ch
 
 # STEP 7 ---------------- compress -------------------------
 # use spec compress or xspec compress for log2 compression
-cpmrs_val0 = spec_compress(avg_pwr0, channel_num=0, show_plots=True, save_output='both')
+cpmrs_val0 = spec_compress(avg_pwr0, channel_num=0, show_plots=False, save_output='both')
 #cpmrs_val1 = spec_compress(avg_pwr1, channel_num=1, show_plots=False, save_output='both')
 
 #cmprs_val_r = xspec_compress(avg_pwr01_r, channel_num=0, show_plots=False, save_output='both')
