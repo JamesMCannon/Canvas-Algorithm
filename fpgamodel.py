a8116d87bf73b077f20714eb20a61f8d400c6d8c
JamesMCannon <james.cannon-1@colorado.edu>
2022-08-02 09:19:07 -0600

    add s-64 save option

--- fpgamodel.py
+++ fpgamodel.py
@@ -39,12 +39,15 @@ n_acc = 8                   # number of FFTs to accummulate
 
 # STEP 1 -------------------- GENERATE INPUT ----------------------------- 
 # get one or two test singals
-channels0_td = test_signal(fs, sample_len, signal_freq0, amp0, shift=shift0, channel_num=0, show_plots=False, save_output=None)
-channels1_td = test_signal(fs, sample_len, signal_freq1, amp1, shift=shift1, channel_num=1, show_plots=False, save_output='hex')
-#inputs = 'Inputs/'
-#freq = '60kHz'
-#file = inputs+'hi_amp_'+freq+'.txt'  
-#channels0_td = read_FPGA_input(file,signed=True,show_plots=True)
+#channels0_td = test_signal(fs, sample_len, signal_freq0, amp0, shift=shift0, channel_num=0, show_plots=False, save_output=None)
+#channels1_td = test_signal(fs, sample_len, signal_freq1, amp1, shift=shift1, channel_num=1, show_plots=False, save_output='hex')
+inputs = 'Inputs/'
+freq = '_24khz'
+phase = "5deg"
+file0 = inputs + 'high-high_'+'0deg'+freq+'.txt'
+file1 = inputs +  'high-high_' + phase + freq + '.txt' 
+channels0_td = read_FPGA_input(file0,signed=True,show_plots=False)
+channels1_td = read_FPGA_input(file1,signed=True,show_plots=False)
 
 #print(max(channels0_td))
 # STEP 2 ----------------- GET HANNING WINDOW ----------------------------
@@ -60,30 +63,33 @@ channel1_fd_real, channel1_fd_imag = canvas_fft(nFFT, fs, win, channels1_td, ove
 # STEP 4 ----------------------- CALC PWR --------------------------------
 # calculate power, diff for spectra and x-spec
 #fpga_rev = "Rev14p1"
-#fpga_file_path = "Data_compare/FPGA_Results/"+fpga_rev+'/FPGA-'+fpga_rev+'_'+freq+'_FFT_hex.txt'
+#fpga_file_path_0 = "Data_compare/xspec_im_test/"+"0712_03khz_spec0_FFT_hex.txt"
+#fpga_file_path_1 = "Data_compare/xspec_im_test/"+"0712_03khz_spec1_FFT_hex.txt"
 
-#f_ar, f_ai = read_FPGA_input_lines(fpga_file_path, 32, line_n=3, x=1, y=2)
+#f_ar_0, f_ai_0 = read_FPGA_input_lines(fpga_file_path_0, 32, line_n=3, x=1, y=2)
+#f_ar_1, f_ai_1 = read_FPGA_input_lines(fpga_file_path_1, 32, line_n=3, x=1, y=2)
 
 spec_pwr0 = fft_spec_power(channel0_fd_real, channel0_fd_imag, channel_num=0, show_plots=False, save_output=None)
 spec_pwr1 = fft_spec_power(channel1_fd_real, channel1_fd_imag, channel_num=1, show_plots=False, save_output=None)
 xspec_pwr_r, xspec_pwr_i = fft_xspec_power(channel0_fd_real, channel0_fd_imag, channel1_fd_real, channel1_fd_imag, channel_nums=[0,1], show_plots=False, save_output=None)
 
-#spec_pwr0 = fft_spec_power(f_ar, f_ai, channel_num=0, show_plots=True, save_output='both')
-#spec_pwr1 = fft_spec_power(f_ar, f_ai, channel_num=1, show_plots=True, save_output='both')
+#spec_pwr0 = fft_spec_power(f_ar_0, f_ai_0, channel_num=0, show_plots=False, save_output='both')
+#spec_pwr1 = fft_spec_power(f_ar_1, f_ai_1, channel_num=1, show_plots=False, save_output='both')
+#xspec_pwr_r, xspec_pwr_i = fft_xspec_power(f_ar_0, f_ai_0, f_ar_1, f_ai_1, channel_nums=[0, 1], show_plots=False,save_output='both')
 #xspec_pwr_r, xspec_pwr_i = fft_spec_power([channel0_fd_real], [channel0_fd_imag], [channel1_fd_real], [channel1_fd_imag], channel_nums=[0,1], show_plots=False, save_output='both')
 # STEP 5 -------------------- rebin and acc -------------------------------
 # functions written to rebin (avg in freq) and acc (avg in time)
 rebin_pwr0= rebin_likefpga(spec_pwr0, channel_num=0, show_plots=False, save_output=None)
 rebin_pwr1= rebin_likefpga(spec_pwr1, channel_num=1, show_plots=False, save_output=None)
 
-rebin_pwr_01_r= rebin_likefpga(xspec_pwr_r, channel_num=0, show_plots=False, save_output=None)
-rebin_pwr_01_i= rebin_likefpga(xspec_pwr_i, channel_num=0, show_plots=False, save_output=None)
+rebin_pwr_01_r= rebin_likefpga(xspec_pwr_r, channel_num=2, show_plots=False, save_output='both')
+rebin_pwr_01_i= rebin_likefpga(xspec_pwr_i, channel_num=3, show_plots=False, save_output='both')
 
 acc_pwr0 = acc_likefpga(rebin_pwr0, n_acc, channel_num=0, show_plots=False, save_output=None)
 acc_pwr1 = acc_likefpga(rebin_pwr1, n_acc, channel_num=1, show_plots=False, save_output=None)
 
-acc_pwr01_r = acc_likefpga(rebin_pwr_01_r, n_acc, channel_num=0, show_plots=False, save_output=None)
-acc_pwr01_i = acc_likefpga(rebin_pwr_01_i, n_acc, channel_num=0, show_plots=False, save_output=None)
+acc_pwr01_r = acc_likefpga(rebin_pwr_01_r, n_acc, channel_num=2, show_plots=False, save_output='both')
+acc_pwr01_i = acc_likefpga(rebin_pwr_01_i, n_acc, channel_num=3, show_plots=False, save_output='both')
 
 # STEP 6 ---------------- average in time and freq -------------------------
 # import canvas bins correctly -- make sure you have this file
--- saveas.py
+++ saveas.py
@@ -19,6 +19,8 @@ def save_output_txt(out_array, out_path, out_type, bits):
                     output.write(format(np.int32(x) & 0xffffffff, '08X') + '\n')
                 if bits == 'u-64':
                     output.write(format(np.uint64(x) & 0xffffffffffffffff, '016X') + '\n')
+                if bits == 's-64':
+                    output.write(format(np.uint64(x) & 0xffffffffffffffff, '016X')+ '\n')    
     if out_type =='int' or out_type =='both':
         out_name = out_path+'_int.txt'
         with open(out_name, 'a') as output:
@@ -31,6 +33,8 @@ def save_output_txt(out_array, out_path, out_type, bits):
                     output.write(str(np.int32(x)) + '\n')
                 if bits == 'u-64':
                     output.write(str(np.uint64(x)) + '\n')
+                if bits == 's-64':
+                    output.write(str(np.int64(x))+ '\n')
     return
 # ------------------------------------------------------------------------------------
 
